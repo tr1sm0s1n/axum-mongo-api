564fdc116ca03369c8cfa2add2214183ccb88f95
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-01-20 22:07:24 +0530

    🎉 | genesis

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use axum::{routing::get, Router};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    // logging middleware
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "axum_mongo_api=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+
+    tracing::debug!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app()).await.unwrap();
+}
+
+fn app() -> Router {
+    Router::new()
+        .route("/", get(home))
+        .layer(TraceLayer::new_for_http())
+}
+
+async fn home() -> &'static str {
+    "Hello, World!"
+}
